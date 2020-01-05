d33316ff78363a18e8912f256caa05d4553a32cc
phonemyatmin-ytuec2019 <phonemyatmin.ytu@outlook.com>
2020-01-05 20:01:07 +0630

    If Else Statement

---
+++ If_Else_Statements.py
@@ -0,0 +1,159 @@
+# Boolean Expression
+
+print(20 > 10)
+print(20 == 10)
+print(20 < 10)
+print(bool("Hello World"))
+print(bool(20))
+
+
+
+
+Python Conditions
+
+Equals 						->	x  ==  y
+Not Equals 					->  x  !=  y
+Less than 					->  x  <   y
+Less than or equal to 		->  x  <=  y
+Greater than 				->  x  >   y
+Greater than  or equal to 	->  x  >=  y
+Boolean OR					->  x  or  y, x | y	 
+Boolean AND 				-> 	x  and y, x & y 
+Boolean NOT					->  not x  
+
+
+
+
+If_Else_Statement.py
+
+
+#If Statement
+
+x = 70 
+y = 60
+
+if x > y:
+	print("x is greater than y")
+
+
+#Elif Statement
+
+x = 70
+y = 70
+
+
+if x > y:
+	print("x is greater than y")
+elif x==y:
+	print("x is equal to y")
+
+
+#Else
+
+x = 50 
+y = 150
+
+
+if x > y:
+	print("x is greater than y")
+elif x == y:
+	print("x and y are equal")
+else:
+	print("x is less than y")
+
+
+#Short Hand If
+
+if x > y: print("x is greater than y")
+
+
+#Short Hand If Else
+
+x = 50
+y = 150
+print(x) if x > y else print(y)
+
+
+
+#And is Logical opreator
+
+x = 50
+y = 40
+z = 100
+if x > y and z > x:
+	print("All Conditions are True")
+else:
+	print("All Conditions are False")
+
+
+#Or is Logical operator
+
+x = 50 
+y = 40
+z = 100
+if x > y or z > y: 
+	print("one of the conditions is True")
+
+
+
+#Nested If is if statements in if statements
+
+x = 50
+
+if x > 10:
+	print("x is greter than ten")
+else:
+	print("x is not ten")
+	if x > 20:
+		print("x is greater than 20")
+	else:
+		print("No, x is not greater than 20")
+
+
+
+if x > 10 and x !=10 and x > 20:
+	print("x is greater than 10 and 20")
+else:
+	print("x is not greater than 10 & 20")
+
+
+#Pass Statement
+
+
+x = 100
+y = 50
+
+
+if x > y:
+	pass
+
+
+
+
+
+----------
+
+
+x = int(input("Please enter an integer: "))
+
+if x < 0:
+	x = 0
+	print('Negative changed to zero')
+elif x == 0:
+	print('Zero')
+elif x == 1:
+	print('Single')
+else:
+	print('More')
+
+
+
+
+
+---------
+
+
+
+
+int(input)
+
